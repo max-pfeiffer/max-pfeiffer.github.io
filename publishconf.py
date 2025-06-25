a31ce9519b08cc6f084c641d45c30f22423de1d5
Max Pfeiffer <max@maxpfeiffer.ch>
2025-06-25 20:43:27 +0200

    Fixed siteurl

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://max-pfeiffer.github.io/blog/"
+SITEURL = "https://max-pfeiffer.github.io/"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
